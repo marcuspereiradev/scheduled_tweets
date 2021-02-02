83a786d5ea598a5b8b57c78d949b1dc5244bb257
Marcus Pereira <marcus.work@hotmail.com>
2021-02-02 19:52:39 -0300

    Creating model User and adding some validations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Adding email and password in database..."
+
+100.times do
+  User.create!(
+    email: Faker::Internet.unique.email,
+    password: Faker::Internet.unique.password
+  )
+end
+
+puts "Added with success"
