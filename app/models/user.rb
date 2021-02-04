9b02857b2301f74159c3414766d7a8e714ad6616
Marcus Pereira <marcus.work@hotmail.com>
2021-02-04 19:53:33 -0300

    Login with sessions cookies and logging out users

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,6 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "Must be a valid email address" }
+  validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address" }
+  validates_uniqueness_of :email, on: :create, message: "must be unique"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  root to: 'main#index'
   get 'about', to: 'about#index'
+  
+  get 'sign_up', to: 'registrations#new'
+  post 'sign_up', to: 'registrations#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  root to: 'main#index'
 end
 
 
